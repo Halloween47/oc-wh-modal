000a3e59e0c542e3f1baea36348539aca6c91698
Thomas Leconte <thomasl.dw20@gmail.com>
2024-02-23 11:41:00 +0100

    Nouveau build

--- dist/components/modal.js
+++ dist/components/modal.js
@@ -11,10 +11,15 @@ var _check = _interopRequireDefault(require("../assets/check.png"));
 var _remove = _interopRequireDefault(require("../assets/remove.png"));
 // import '../App.css'
 
+// Définition d'un composant fonctionnel nommé "Modal" prenant deux props : onClose et etat
 function Modal(_ref) {
   var onClose = _ref.onClose,
     etat = _ref.etat;
-  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, etat ? /*#__PURE__*/_react["default"].createElement("div", {
+  // Retourne le contenu du composant Modal
+  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, etat ?
+  /*#__PURE__*/
+  // Si "etat" est true, affiche cette section
+  _react["default"].createElement("div", {
     id: "confirmation",
     className: "modal"
   }, /*#__PURE__*/_react["default"].createElement("div", {
@@ -24,7 +29,10 @@ function Modal(_ref) {
     alt: "check"
   }), /*#__PURE__*/_react["default"].createElement("p", null, "Employ\xE9 ajout\xE9 avec succ\xE8s"), /*#__PURE__*/_react["default"].createElement("br", null), /*#__PURE__*/_react["default"].createElement("button", {
     onClick: onClose
-  }, "Fermer le modal"))) : /*#__PURE__*/_react["default"].createElement("div", {
+  }, "Fermer le modal"))) :
+  /*#__PURE__*/
+  // Si "etat" est false, affiche cette section
+  _react["default"].createElement("div", {
     id: "confirmation",
     className: "modal"
   }, /*#__PURE__*/_react["default"].createElement("div", {
@@ -36,4 +44,6 @@ function Modal(_ref) {
     onClick: onClose
   }, "Fermer"))));
 }
-var _default = exports["default"] = Modal;
+
+// Spécification des types des props attendues par le composant Modal
+var _default = exports["default"] = Modal; // Export du composant Modal
